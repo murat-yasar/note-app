cc4f2422b56d59f7b91d6246fdabe327f3d1e8e2
Murat Yaşar <dev.muratyasar@gmail.com>
2025-03-27 18:49:36 +0100

    Add Note class

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
-class Task {
+class Note {
 	constructor() {
+      this.notes = [];
+
 		this.$form = document.querySelector("#form");
 		this.$noteTitle = document.querySelector("#note-title");
+      this.$noteText = document.querySelector("#note-text");
 		this.$formButtons = document.querySelector("#form-buttons");
 
 		this.addEventListeners();
@@ -11,6 +14,15 @@ class Task {
 		document.body.addEventListener("click", (event) => {
 			this.handleFormClick(event);
 		});
+      this.$form.addEventListener("submit", (event) => {
+         event.preventDefault();
+         const title = this.$noteTitle.value;   
+         const text = this.$noteText.value;
+         const hasNote = title || text;
+         if (hasNote) {
+            this.addNote({ title, text });
+         }
+      });
 	}
 
 	handleFormClick(event) {
@@ -36,6 +48,17 @@ class Task {
 		this.$noteTitle.style.display = "none";
 		this.$formButtons.style.display = "none";
 	}
+
+   addNote(note) {
+    const newNote = {
+      title: note.title,
+      text: note.text,
+      color: 'white',
+      id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
+    };
+    this.notes = [...this.notes, newNote];
+    console.log(this.notes);
+  }
 }
 
-new Task();
+new Note();
